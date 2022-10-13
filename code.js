c00c006aa6015ab533d3f131399ade949697de23
Joshua Ramos <joshua.m.ramos32@gmail.com>
2022-10-13 11:10:45 -0500

    adding exercise 1

--- code.js
+++ code.js
@@ -2,3 +2,15 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function sayHello(){
+    return "hello";
+}
+/*function subtract(x, y){
+    if(typeof x === "number" && typeof y === "number"){
+        return x -y;
+    } else{
+        return false;
+
+    }
+}*/
--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 // Unit tests for the helloWorld function
 describe('helloWorld', function() {
-    it('should be a defined function', function() {
+    it('should be a defined function within code.js', function() {
       expect(typeof helloWorld).toBe('function');
     });
     it('should return a string when called', function() {
@@ -13,3 +13,25 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+describe("hello", function(){
+    it("should return a defined function", function(){
+        expect(sayHello()).toBe("hello")
+    })
+})
+
+/* only have one test per spec!
+describe("subtract", function(){
+   it("should be a defined function", function(){
+       expect(typeof subtract).toBe("function");
+   });
+    it("should return a number when passed to number arguments",function(){
+        expect(typeof subtract(3, 1)).toBe("number");
+    });
+    it("should return false when passed any argument that is not a number", function (){
+        expect(subtract()).toBe(false);
+    });
+    it("should return 34 when passed the argument of 64 and 30", function(){
+		expect(subtract(64, 30)).toBe(34);
+	})
+});*/
+
